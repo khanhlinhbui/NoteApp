34c54a8e9088bf17031e1883ebba07c11454e932
linh bui <miumiubunni@linhs-MacBook-Pro.local>
2023-05-18 17:31:39 -0700

    ignore files

--- server.js
+++ server.js
@@ -6,6 +6,5 @@ const PORT = 3001; // door for server
 
 app.use(express.static('public')); // allow express.js the ability to use static file ( images, CSS)
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html') ))
-app.listen(PORT, () => 
-    console.log(' cat eats fish ') // express will set up server and listen to any requests and responses
+app.listen(PORT, () => console.log('hihi') // express will set up server and listen to any requests and responses
 );
