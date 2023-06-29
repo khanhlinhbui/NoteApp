c8d352701eafaddafc3d2d424cb9d83405e30bd8
linh bui <miumiubunni@linhs-MacBook-Pro.local>
2023-06-30 00:36:15 +0700

    defined POST api not request

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3001; // door for server
 
 app.use(express.static('public')); // allow express.js the ability to use static file ( images, CSS)
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html') ))
-app.get('/notes', (req, res)=> res.sendFile(path.join(__dirname,'public/notes.html')))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,'public/notes.html'))) // link index.html and notes.html
+app.post('/api/notes', (req, res) => console.log('12345'))
 app.listen(PORT, () => console.log('hihi') // express will set up server and listen to any requests and responses
 );
